a99925ee29ed9ea77cc854c4dad347ed9305ac05
DanielJMus <danielmus1999@hotmail.com>
2020-06-12 11:46:46 +1200

    Set up SelfHost service

---
+++ SelfHostServer/PianoStoreController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SelfHostServer
+{
+	public class PianoStoreController : System.Web.Http.ApiController
+	{
+		public List<string> GetArtistNames()
+		{
+			DataTable lcResult = ClsDBConnection.GetDataTable("SELECT Name FROM Artist", null);
+			List<string> lcNames = new List<string>();
+			foreach(DataRow dr in lcResult.Rows)
+			{
+				lcNames.Add((string)dr[0]);
+			}
+			return lcNames;
+		}
+	}
+}
---
+++ SelfHostServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Uri _baseAddress = new Uri("http://localhost:60064/");
+			HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+			config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+			HttpSelfHostServer server = new HttpSelfHostServer(config);
+			// Start Listening
+			server.OpenAsync().Wait();
+			Console.WriteLine("Piano Store Web-API Self-hosted on " + _baseAddress);
+			Console.WriteLine("Hit ENTER to exit..");
+			Console.ReadLine();
+			server.CloseAsync().Wait();
+		}
+	}
+}
